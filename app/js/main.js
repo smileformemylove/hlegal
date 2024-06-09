32824dd0fc1af59bea45e0c7e3cd69a16a157dc4
Mykola <Shekezzz@gmail.com>
2024-06-09 20:50:16 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+(() => {
+	const burger = document.querySelector('.header__box-burger');
+	const close = document.querySelector('.header__nav-close');
+	const menu = document.querySelector('.header__nav');
+
+	burger.addEventListener('click', () => {
+		menu.classList.add('active')
+	});
+
+	close.addEventListener('click', () => {
+		menu.classList.remove('active')
+	})
+})();
+
+(() => {
+	const tabItems = document.querySelectorAll('.tabs__links-item')
+	const tabBlocks = document.querySelectorAll('.tabs__contents-item')
+
+	tabItems.forEach((tab, index) => {
+		tab.addEventListener('click', (event) => {
+			tabBlocks.forEach(content => {
+				content.classList.remove('active')
+			})
+			tabItems.forEach(tab => {
+				tab.classList.remove('active')
+			})
+			tabItems[index].classList.add('active')
+			tabBlocks[index].classList.add('active')
+		})
+	})
+})();
